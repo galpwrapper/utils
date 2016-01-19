781d2e0ea8ddbe35b6cdc539f054621210a1a6dc
chwei <weicc1989@gmail.com>
2016-01-19 16:47:32 +0100

    add PRINT_Divding_line to oformat

--- oformat/oformat.h
+++ oformat/oformat.h
@@ -17,3 +17,13 @@ The format to out put numbers
 #ifndef FORMATE4
 #define FORMATE4 "%.5e %.5e %.5e %.5e\n"
 #endif
+
+#ifndef PRINT_Dividing_line
+#include <iostream>
+#include <iomanip> 
+#define PRINT_Dividing_line(flag)\
+  std::cout<<std::setfill(flag)\
+           <<std::setw(79)\
+           <<flag\
+           <<std::endl
+#endif
