50389cee376d6d17cd28dd20d994f2d2347833f0
linsj <lin.sujie1234@gmail.com>
2017-10-23 16:11:52 +0800

    add NO_ROOT support for locker.cc

--- locker/locker.cc
+++ locker/locker.cc
@@ -1,5 +1,9 @@
 #include "locker.h"
+#ifdef NO_ROOT
+#include <fstream>
+#else
 #include "TFile.h"
+#endif
 
 #include <iostream>
 
@@ -9,8 +13,12 @@ locker::locker(const string &filename, int lock_type) : file(0), fd(0), availabl
   file = fopen(filename.c_str(), "r");
   if (!file) {
     if (LOCK_EX == lock_type) {
+#ifdef NO_ROOT
+      ofstream(filename.c_str());
+#else
       TFile fnew(filename.c_str(), "UPDATE");
       fnew.Close();
+#endif
       file = fopen(filename.c_str(), "r");
     } else {
       cout << "File " << filename << " unexist, do nothing" << endl;
