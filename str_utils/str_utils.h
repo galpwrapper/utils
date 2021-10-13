dd5bbc045cbde107538024e5e8ed901b8a58d3ae
linsj <lin.sujie1234@gmail.com>
2021-10-13 14:24:09 +0800

    add split<T>() function

--- str_utils/str_utils.h
+++ str_utils/str_utils.h
@@ -11,6 +11,21 @@
 #include <climits>
 
 std::vector<std::string> split(const std::string& str, const std::string& splitor = " ");
+template<typename T>
+std::vector<T> split(const std::string& str, const std::string& splitor = " ") {
+  auto strvec = split(str, splitor);
+  std::vector<T> result;
+  result.reserve(strvec.size());
+  T tmp;
+
+  for (auto str : strvec) {
+    std::istringstream is(str);
+    is >> tmp;
+    result.push_back(tmp);
+  }
+
+  return result;
+}
 
 template <typename T>
 std::string join(const std::vector<T>& vecs, const std::string& splitor = " ", int istart = 0, int iend = -1) {
